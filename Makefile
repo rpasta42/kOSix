4534fc1f8608274773f32172d8e18f67fcc3a412
Konstantin Kowalski <kostelkow@gmail.com>
2017-07-26 16:40:03 -0400

    GDT/IDT/etc

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 
 #run: source ./setup_cross_comp.sh
 
+
+CC=i686-elf-gcc
+CFLAGS=-std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
 default: test
 
 test: kosix.bin
@@ -12,11 +16,17 @@ test: kosix.bin
 #iso: kosix.bin
 #	mv kosix.bin isodir/boot/; grub-mkrescue -o kosix.iso isodir
 
-kosix.bin: kernel.o boot.o
-	i686-elf-gcc -T linker.ld -o kosix.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
-kernel.o: #setup-cross-comp
-	i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+%.o: %.c
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+
+kosix.bin: gdt.o utils.o term.o idt.o kernel.o boot.o
+	$(CC) -T linker.ld -o kosix.bin -ffreestanding -O2 -nostdlib *.o -lgcc
+
+
+#kernel.o: #setup-cross-comp
+#	$(CC) -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 boot.o:
 	nasm -felf32 boot.asm -o boot.o
--- kernel.h
+++ kernel.h
@@ -1,8 +1,69 @@
+#ifndef KERNEL_H_INCLUDE
+#define KERNEL_H_INCLUDE
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* Check if the compiler thinks we are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+
+/* This tutorial will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
+
+/*************************VGA*************************/
+
+/* Hardware text mode color constants. */
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+	return fg | bg << 4;
+}
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+
+/*********************END VGA*************************/
+
+
+
+/*************************GDT*************************/
+
+void gdt_install();
+
+//http://www.osdever.net/bkerndev/Docs/gdt.htm
+
+
 /* Defines a GDT entry. We say packed, because it prevents the
 *  compiler from doing things that it thinks is best: Prevent
 *  compiler "optimization" by packing */
-struct gdt_entry
-{
+struct gdt_entry {
     unsigned short limit_low;
     unsigned short base_low;
     unsigned char base_middle;
@@ -13,8 +74,7 @@ struct gdt_entry
 
 /* Special pointer which includes the limit: The max bytes
 *  taken up by the GDT, minus 1. Again, this NEEDS to be packed */
-struct gdt_ptr
-{
+struct gdt_ptr {
     unsigned short limit;
     unsigned int base;
 } __attribute__((packed));
@@ -24,3 +84,60 @@ struct gdt_entry gdt[3];
 struct gdt_ptr gp;
 
 extern void gdt_flush();
+
+/*********************END GDT*************************/
+
+
+/*************************IDT*************************/
+
+void idt_install();
+
+
+/* Defines an IDT entry */
+struct idt_entry
+{
+    unsigned short base_lo;
+    unsigned short sel;        /* Our kernel segment goes here! */
+    unsigned char always0;     /* This will ALWAYS be set to 0! */
+    unsigned char flags;       /* Set using the above table! */
+    unsigned short base_hi;
+} __attribute__((packed));
+
+struct idt_ptr
+{
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+/* Declare an IDT of 256 entries. Although we will only use the
+*  first 32 entries in this tutorial, the rest exists as a bit
+*  of a trap. If any undefined IDT entry is hit, it normally
+*  will cause an "Unhandled Interrupt" exception. Any descriptor
+*  for which the 'presence' bit is cleared (0) will generate an
+*  "Unhandled Interrupt" exception */
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+
+/*********************END IDT*************************/
+
+
+/*************************UTILS*************************/
+
+size_t strlen(const char* str);
+unsigned char *memset(unsigned char *dest, unsigned char val, int count);
+
+/*********************END UTILS*************************/
+
+
+/*************************TERM*************************/
+
+void terminal_initialize(void);
+void terminal_writestring(const char* data);
+
+/*********************END TERM*************************/
+
+
+
+#endif //KERNEL_H_INCLUDE
+
